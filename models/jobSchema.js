745fba4e34dad9c562f8a90c3dc3bd1ba441d7f0
webpandaaa <ankushrajsingh2004@gmail.com>
2024-05-29 00:09:48 +0530

    Day 4 Schema complete

--- models/jobSchema.js
+++ models/jobSchema.js
@@ -13,4 +13,48 @@ const jobSchema = new mongoose.Schema({
         minLength : [3, "job description must contain at least 3 characeters"],
         maxLength : [350, "job description  cannot exceed 350 characeters"],
     },
-})
+    category : {
+        type : String,
+        required : [true , "please provide job category"],
+    },
+    country : {
+        type : String,
+        required : [true , "please provide job category"],
+    },
+    city : {
+        type : String
+    },
+    location : {
+        type : String,
+        required : [true , "please provide job location"],
+    },
+    fixedSalary:{
+        type : Number,
+        minLength : [4, "Fixed Salary must contain at least 4 characeters"],
+        maxLength : [9, "Fixed Salary cannot exceed 9 characeters"],
+    },
+    salaryFrom:{
+        type : Number,
+        minLength : [4, "Fixed Salary must contain at least 4 characeters"],
+        maxLength : [9, "Fixed Salary cannot exceed 9 characeters"],
+    },
+    SalaryTo:{
+        type : Number,
+        minLength : [4, "Fixed Salary must contain at least 4 characeters"],
+        maxLength : [9, "Fixed Salary cannot exceed 9 characeters"],
+    },
+    expired:{
+        type : Boolean,
+        default : false,
+    },
+    jobPostedOn : {
+        type:Date,
+        default : Date.now,
+    },
+    postedBy:{
+        type : mongoose.Schema.ObjectId,
+        ref : "User",
+        required : true,
+    },
+});
+
